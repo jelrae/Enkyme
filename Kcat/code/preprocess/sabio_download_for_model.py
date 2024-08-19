3c3edaf8592fcd1d6cffc332a05ea90163bb3d27
L-bytes <marlenevlaar@outlook.com>
2024-08-19 18:05:06 +0200

    "Final code clean-up + data files for kcat"

---
+++ Kcat/code/preprocess/sabio_download_for_model.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python
+
+import requests
+from time import sleep
+from ete3 import NCBITaxa
+ncbi = NCBITaxa()
+
+def is_seed_plant(org):
+    try:
+        tax_id = ncbi.get_name_translator([org])[org][0]
+        lineage = ncbi.get_lineage(tax_id)
+        if 58024 not in lineage:
+            return(False)
+        else:
+            return(True)
+    except KeyError:
+        return(False)
+
+organism = "Seed plants"
+
+
+def eclist():
+    QUERY_URL = 'http://sabiork.h-its.org/sabioRestWebServices/kineticlawsExportTsv'
+
+    query = {'fields[]':['ECNumber'], 'q':'Parametertype:"kcat"'}
+
+    request = requests.post(QUERY_URL, params = query)
+
+    results = request.text
+
+    ECs = [x for x in results.strip().split('\n')][1:]
+
+    return sorted(list(set(ECs)))[1:]
+
+def sabio_info(allEC):
+    i = 0
+    with open('../../Data/EC_kcat_model_' + organism + '.tsv', 'w') as ECfile :
+        records = ['ECs', 'Organism', 'Uniprot IDs', 'PMID', 'Type', 'kcat', 'Temperature', 'pH', 'Substrates', 'Products', 'substrate_IDs', 'product_IDs', 'Main Substrate']
+        ECfile.write('\t'.join(records) + '\n')
+
+    with open('../../Data/max_EC_' + organism + '.tsv', 'w') as file :
+        file.write('\t'.join(['EC', "max_kcat"]) + '\n')
+
+    ids = {}
+    for EC in allEC:
+        QUERY_URL = 'https://sabiork.h-its.org/sabioRestWebServices/kineticlawsExportTsv'
+        i += 1
+        print('This is %s ----------------------------' %EC)
+        query_dict = {"ECNumber":'%s' %EC,"Organism":'*'}
+        query_string = ' AND '.join(['%s:%s' % (k,v) for k,v in query_dict.items()])
+
+        query = {'fields[]':['Organism', 'Substrate', 'Product', 'UniprotID', 'EnzymeType', 'PubMedID', 'pH', 'Temperature', 'Parameter'], 'format':'tsv', 'q':query_string}
+        
+        try:
+            request = requests.post(QUERY_URL, params = query)
+        except requests.exceptions.SSLError:
+            sleep(5)
+            try:
+                request = requests.post(QUERY_URL, params = query)
+            except requests.exceptions.SSLError:
+                continue
+
+        results = request.text
+        print('---------------------------------------------')
+
+        if results:
+            max_kcat=list()
+            for line in results.strip().split('\n')[1:]:
+                substrateids = []
+                productids = []
+                complete = True
+                entry = line.split('\t')
+                
+                if not is_seed_plant(entry[0]):
+                    continue
+
+                if entry[8] != "kcat" or not entry[10]:
+                    continue
+                
+                if entry[13] == 'm^(-1)':
+                    entry[10] = str(float(entry[10])/60)
+                    entry[13] = 's^(-1)'
+
+                if entry[13] != 's^(-1)':
+                    continue
+
+                max_kcat.append(float(entry[10]))
+
+                substrates=entry[1]
+                products=entry[2]
+
+                main_substrate = 'None'
+
+                count=0
+                for line2 in results.strip().split('\n')[1:]:
+                    entry2 = line2.split('\t')
+                    if entry2[8] == 'kcat/Km' and entry2[:7] == entry[:7]:
+                        if entry[9] != main_substrate:
+                                count += 1
+                        main_substrate = entry2[9]
+                if count == 0:
+                    for line2 in results.strip().split('\n')[1:]:
+                        entry2 = line2.split('\t')
+                        if entry2[8] == 'Km' and entry2[:7] == entry[:7]:
+                            if entry[9] != main_substrate:
+                                count += 1
+                            main_substrate = entry2[9]
+                if count != 1 or main_substrate not in substrates:
+                    complete=False
+
+                for c in (substrates+';'+products).split(';'):
+                    compound = ''
+                    compoundid = ''
+                    compoundinfo = ''
+                    if c not in ids.keys():
+                        QUERY_URL = 'https://sabiork.h-its.org/sabioRestWebServices/searchCompoundSynonyms'
+                        query = {'fields[]':["SabioCompoundID"], "CompoundName":c}
+                        try:
+                            compound = (requests.get(QUERY_URL, params = query)).text
+                        except requests.exceptions.SSLError:
+                            sleep(5)
+                            try:
+                                compound = (requests.get(QUERY_URL, params = query)).text
+                            except requests.exceptions.SSLError:
+                                complete=False
+                        try:
+                            compoundid = compound.strip().split('\n')[1]
+                        except:
+                            complete = False
+
+                        QUERY_URL = 'https://sabiork.h-its.org/sabioRestWebServices/searchCompoundDetails'
+                        query = {'fields[]':["KeggCompoundID", "InChI"], "SabioCompoundID":compoundid}
+                        try:
+                            compoundinfo = (requests.get(QUERY_URL, params = query)).text
+                        except requests.exceptions.SSLError:
+                            sleep(5)
+                            try:
+                                compoundinfo = (requests.get(QUERY_URL, params = query)).text     
+                            except requests.exceptions.SSLError:
+                                complete=False                     
+                        try:  
+                            compoundreps = compoundinfo.strip().split('\n')[1]
+                            compoundrep = compoundreps.split('\t')
+                            if compoundrep[1] and compoundrep[1] != 'null':
+                                ids[c] = compoundrep[1]
+                            elif compoundrep[0] and compoundrep[0] != 'null':
+                                ids[c] = compoundrep[0]
+                            else:
+                                complete=False
+                        except:
+                            complete = False
+
+                if (complete == False):
+                    continue
+                else:
+                    print(line)
+                    
+                    for s in substrates.split(';'):
+                        substrateids.append(ids[s])
+
+                    for p in products.split(';'):
+                        productids.append(ids[p])
+
+                    substrateids = sorted(substrateids)
+                    productids = sorted(productids)
+
+                    if entry[3]:
+                        with open('../../Data/EC_kcat_model_' + organism + '2.tsv', 'a') as ECfile :
+                            ECfile.write('\t'.join([EC, entry[0], ';'.join(list(set(entry[3].split(' ')))), entry[5], entry[4], entry[10], entry[7], entry[6],
+                             substrates, products, '#'.join(substrateids), '#'.join(productids), ids[main_substrate]]) + '\n')
+                        
+            if max_kcat:
+                print(max_kcat)
+                with open('../../Data/max_EC_' + organism + '2.tsv', 'a') as ECfile :
+                        ECfile.write('\t'.join([EC, str(max(max_kcat))]) + '\n')
+
+if __name__ == '__main__' :
+    allEC = eclist()
+    print(allEC)
+    sabio_info(allEC)
+
+
--- Kcat/code/preprocess/uniprot_sequence.py
+++ Kcat/code/preprocess/uniprot_sequence.py
@@ -1,54 +1,35 @@
 #!/usr/bin/python
 # coding: utf-8
 
-# Author: LE YUAN
-# Date: 2020-06-25
-
-# This python script is to obtain protein sequence by uniprot protein id
-
 from urllib import request
 import requests
-import csv
 import re
 from zeep import Client
 import hashlib
 
 organism = "Seed plants"
 
-# This function is to obtain the protein sequence according to the protein id from Uniprot API
-# https://www.uniprot.org/uniprot/A0A1D8PIP5.fasta 
-# https://www.uniprot.org/help/api_idmapping
 def uniprot_sequence(id) :
     url = "https://www.uniprot.org/uniprot/%s.fasta" % id
     IdSeq = dict()
-
     try :
         data = request.urlopen(url)
         respdata = data.read().decode("utf-8").strip()
         IdSeq[id] =  "".join(respdata.split("\n")[1:])
     except :
-        print(id, "can not find from uniprot!")
+        print(id, "Cannot find from uniprot!")
         IdSeq[id] = None
     print(IdSeq[id])
     return IdSeq[id]
 
 def seq_by_ec_organism(ec, organism) :
     IdSeq = dict()
-    # https://www.biostars.org/p/356687/
     params = {"query": "ec:%s AND organism:%s AND reviewed:yes" % (ec, organism), "format": "fasta"}
     response = requests.get("http://www.uniprot.org/uniprot/", params=params)
-    # print(type(response.text)) # <class 'str'>
 
     try :
-        # respdata = response.text.strip()
-        # # print(respdata) 
-        # IdSeq[ec+'&'+organism] =  "".join(respdata.split("\n")[1:])
-
         respdata = response.text
-        # print(respdata)
-        sequence = list()
         seq = dict()
-        i = 0
         for line in respdata.split('\n') :
             if line.startswith('>') :
                 name=line
@@ -58,29 +39,24 @@ def seq_by_ec_organism(ec, organism) :
         IdSeq[ec+'&'+organism] =  list(seq.values())
 
     except :
-        print(ec+'&'+organism, "can not find from uniprot!")
+        print(ec+'&'+organism, "Cannot find from uniprot!")
         IdSeq[ec+'&'+organism] = None
 
     print(IdSeq[ec+'&'+organism])
     return IdSeq[ec+'&'+organism]
 
 def seq_by_brenda(ec, organism) :
-    # E-mail in BRENDA:
-    email = 'm.van.laar@student.vu.nl'
-    # Password in BRENDA:
-    password = 'BRENDAenzyming'
+    email = ''
+    password = ''
 
     wsdl = "https://www.brenda-enzymes.org/soap/brenda_zeep.wsdl"
-    password    = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    password = hashlib.sha256(password.encode("utf-8")).hexdigest()
     client = Client(wsdl)
-    # credentials = email + ',' + password
 
-    # parameters = credentials+","+"ecNumber*%s#organism*%s" %(ec, organism)
-    parameters = ( email,password,"ecNumber*%s" % ec,"organism*%s" % organism, "sequence*", "noOfAminoAcids*", "firstAccessionCode*", "source*Swiss-Prot", "id*" ) # *Swiss-Prot
+    parameters = (email,password,"ecNumber*%s" % ec,"organism*%s" % organism, "sequence*", "noOfAminoAcids*", "firstAccessionCode*", "source*Swiss-Prot", "id*" )
     entries = client.service.getSequence(*parameters)
 
     sequences = list()
-    # print(split_sequences)
     if entries :
         for entry in entries :
             sequences.append(entry['sequence'])
@@ -114,7 +90,7 @@ def main() :
                             outfile.write('\t'.join([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], seq]) + '\n')
                     else:
                         try:
-                            mutantSites = re.findall('[A-Z]\d+[A-Z]', data[4])  # re is of great use
+                            mutantSites = re.findall('[A-Z]\d+[A-Z]', data[4])
                             mutant1_1 = [mutantSite[1:-1] for mutantSite in mutantSites]
                             mutant1_2 = [mutantSite for mutantSite in mutantSites]
                             mutant1 = [mutant1_1, mutant1_2]
